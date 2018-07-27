a98e7337df94767a01a8fc918488992bcc5898b1
Alvaro Gala <agala@stratio.com>
2018-07-27 08:40:42 +0200

    test recover open tabs to console

--- jupyterLab/jupyterlab-docker/jupyterlab/jupyter_spark_ui/src/main/jupyter_spark_ui/static/jupyterlab-plugin.js
+++ jupyterLab/jupyterlab-docker/jupyterlab/jupyter_spark_ui/src/main/jupyter_spark_ui/static/jupyterlab-plugin.js
@@ -6,18 +6,28 @@ import {
     Widget
 } from '@phosphor/widgets';
 
+import { each } from '@phosphor/algorithm';
+
+import {
+    ILayoutRestorer,
+    JupyterLab,
+    JupyterLabPlugin
+} from '@jupyterlab/application';
+
 var common = require('./extension.js');
 
 const plugin = {
     id: 'jupyter_spark_ui',
     autoStart: true,
-    activate: (app) => {
+    requires: [ILayoutRestorer],
+    activate: (app, restorer) => {
         let api_url = app.serviceManager.serverSettings.baseUrl + "/../spark/api/v1";
         let widget = new Widget();
         widget.id = 'jupyter-spark';
         widget.class = 'jupter-spark-panel';
         widget.title.label = 'Spark';
         widget.title.closable = true;
+        restorer.add(widget, 'jupyter-spark');
 
         widget.onBeforeShow = (msg) => {
             widget.intervalId = window.setInterval(common().update, 500, api_url);
@@ -32,6 +42,32 @@ const plugin = {
         widget.node.appendChild(div);
 
         app.shell.addToLeftArea(widget);
+
+
+        // test to catch all open tabs
+        app.restored.then(function () {
+            var populate = function () {
+                //tabs.clearTabs();
+                each(app.shell.widgets('main'), function (widget) {
+                    console.log(widget.title);
+                    console.log(api_url);
+                });
+            };
+            // Connect signal handlers.
+            app.shell.layoutModified.connect(function () {
+                populate();
+            });
+
+         /*   tabs.tabActivateRequested.connect(function (sender, tab) {
+                shell.activateById(tab.title.owner.id);
+            });
+            tabs.tabCloseRequested.connect(function (sender, tab) {
+                tab.title.owner.close();
+            });
+         */
+            // Populate the tab manager.
+            populate();
+        });
     }
 };
 
